8ca3f33e272f6e6dc70cf498c177e718d538023e
khouloudFF <khouloud.haddad@gmail.com>
2022-12-24 20:58:04 +0100

    combos slider

--- src/app/components/homeSections/combos/combos.component.ts
+++ src/app/components/homeSections/combos/combos.component.ts
@@ -10,64 +10,90 @@ export class CombosComponent {
     { 
       title: "Combo 1",
       product: "Classic Burger",
-      content: {
-        item1: "chocolate",
-        item2: "Milkshake"
-      },
+      content: [
+       "chocolate",
+        "Milkshake"
+      ],
       output: "New York Cheesecake",
       price: "€00"
      },
     { 
       title: "Combo 2",
       product: "Cheese Burger",
-      content: {
-        item1: "Strawberry",
-        item2: "Milkshake"
-      },
+      content: [
+       "chocolate",
+        "Milkshake"
+      ],
       output: "Berry Mouffin",
       price: "€00"
      },
     { 
       title: "Combo 3",
       product: "Double Cheese Burger",
-      content: {
-        item1: "Banara Milkshake"
-      },
+      content: [
+      "Banara Milkshake"
+      ],
+     
       output: "Sacher Cake",
       price: "€00"
     },
 
-    { 
+    {
       title: "Combo 4",
       product: "Classic Burger",
-      content: {
-        item1: "chocolate",
-        item2: "Milkshake"
-      },
+      content: [
+       "chocolate",
+        "Milkshake"
+      ],
       output: "New York Cheesecake",
       price: "€00"
      },
     { 
       title: "Combo 5",
       product: "Cheese Burger",
-      content: {
-        item1: "Strawberry",
-        item2: "Milkshake"
-      },
+      content: ["Strawberry", "Milkshake"],
       output: "Berry Mouffin",
       price: "€00"
      },
     { 
       title: "Combo 6",
       product: "Double Cheese Burger",
-      content: {
-        item1: "Banara Milkshake"
-      },
+      content: [
+       "chocolate",
+        "Milkshake"
+      ],
       output: "Sacher Cake",
       price: "€00"
     }
   ];
-  slideConfig = { slidesToShow: 3, slidesToScroll: 1, "infinite": true , "arrows": true,};
+  slideConfig = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+  };
  
     slickInit(e: any) {
       console.log('slick initialized');
