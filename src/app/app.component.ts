a3a8b2e777a7918d71cfc5cc3219111e05916f8b
khouloudFF <khouloud.haddad@gmail.com>
2022-12-24 21:15:22 +0100

    fixedTop menu on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'americanBurger';
+
+  ngOnInit()
+  {
+    window.addEventListener('scroll', function() {
+      navbarScroll();
+     });
+     function navbarScroll() {
+       var y = window.scrollY;
+       if (y > 10) {
+         var header = document.getElementsByTagName('header')[0];
+           header.classList.add('fixedTop');
+     
+       } else if (y < 10) {
+           var header = document.getElementsByTagName('header')[0];
+          header.classList.remove('fixedTop');
+       }
+     }
+  }
 }
